8c38fa0ba8054c652e6882cff3b641b8110900d8
sus0985 <sus0985@gmail.com>
2021-05-25 17:46:16 +0900

    Change

--- 스타트와 링크/스타트와 링크/main.cpp
+++ 스타트와 링크/스타트와 링크/main.cpp
@@ -5,51 +5,51 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	vector<vector<int>> a(n, vector<int>(n));
+    vector<vector<int>> a(n, vector<int>(n));
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> a[i][j];
-		}
-	}
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
 
-	vector<int> b(n);
+    vector<int> b(n);
 
-	for (int i = 0; i < n / 2; i++) {
-		b[i] = 1;
-	}
+    for (int i = 0; i < n / 2; i++) {
+        b[i] = 1;
+    }
 
-	sort(b.begin(), b.end());
+    sort(b.begin(), b.end());
 
-	int ans = 2147483647;
+    int ans = 2147483647;
 
-	do {
-		vector<int> first, second;
-		for (int i = 0; i < n; i++) {
-			if (b[i] == 0) {
-				first.push_back(i);
-			} else{
-				second.push_back(i);
-			}
-		}
-		int one = 0;
-		int two = 0;
+    do {
+        vector<int> first, second;
+        for (int i = 0; i < n; i++) {
+            if (b[i] == 0) {
+                first.push_back(i);
+            } else{
+                second.push_back(i);
+            }
+        }
+        int one = 0;
+        int two = 0;
 
-		for (int i = 0; i < n / 2; i++) {
-			for (int j = 0; j < n / 2; j++) {
-				if (i == j) continue;
-				one += a[first[i]][first[j]];
-				two += a[second[i]][second[j]];
-			}
-			
-		}
-		int diff = one - two;
-		if (diff < 0) diff = -diff;
-		if (ans > diff) ans = diff;
-	} while (next_permutation(b.begin(), b.end()));
-	cout << ans;
-	return 0;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                if (i == j) continue;
+                one += a[first[i]][first[j]];
+                two += a[second[i]][second[j]];
+            }
+            
+        }
+        int diff = one - two;
+        if (diff < 0) diff = -diff;
+        if (ans > diff) ans = diff;
+    } while (next_permutation(b.begin(), b.end()));
+    cout << ans;
+    return 0;
 }
